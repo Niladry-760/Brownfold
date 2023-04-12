ac256ca72b7fc5bbc68c6cd239ad10d46e559856
Niladry <niladry.kar@shyamfuture.com>
2023-04-12 07:46:20 +0530

    DOcument cookie added and details page added

--- products/urls.py
+++ products/urls.py
@@ -9,5 +9,12 @@ app_name = 'products'
 urlpatterns = [
 
     url(r'^list/$',views.all_product_list, name='all_product_list'),
+    url(r'^category_wise_product/$',views.search_product_category_wise, name='search_product_category_wise'),
+
+    url(r'^category_product_details/(?P<category_slug>[\w-]+)/$',
+        views.product_category_details_view, name='product_category_details'),
+
+    url(r'^(?P<product_slug>[\w-]+)/$',
+        views.product_details_view, name='product_detail'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
 
 from .models import *
 
@@ -51,6 +51,43 @@ def product_category_details_view(request, category_slug):
 	except EmptyPage:
 		product_list = paginator.page(paginator.num_pages)
 
+	category_list = ProductCategory.objects.all().order_by('id')
+
+	context = {
+		'category_details' : category_details,
+		'product_list' : product_list,
+		'category_list' : category_list
+	}
+	return render(request, 'product_category_details.html', context)
+
+
+def search_product_category_wise(request):
+	"""
+	Search Product Category Wise
+	"""
+	category_slug = request.GET.get('q')
+
+
+	if category_slug == 'all':
+
+		return redirect(reverse('product:all_product_list'))
+
+	else:
+		category_details = ProductCategory.objects.filter(slug=category_slug).first()
+
+		products = Product.objects.filter(category=category_details).order_by('-id')
+	
+
+	page = request.GET.get('page', 1)
+
+	paginator = Paginator(products, 16)
+	try:
+		product_list = paginator.page(page)
+	except PageNotAnInteger:
+		product_list = paginator.page(1)
+	except EmptyPage:
+		product_list = paginator.page(paginator.num_pages)
+
 	category_list = ProductCategory.objects.all().exclude(id=category_details.id).order_by('id')
 
 	context = {
@@ -59,3 +96,15 @@ def product_category_details_view(request, category_slug):
 		'category_list' : category_list
 	}
 	return render(request, 'product_category_details.html', context)
+
+
+def product_details_view(request, product_slug):
+	"""
+	Product Details View
+	"""
+	product_details = Product.objects.filter(slug=product_slug).first()
+
+	context = {
+		'product_details' : product_details
+	}
+	return render(request, 'product_details.html', context)
